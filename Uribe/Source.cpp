869be38cd1508b820be482418e9ee0b65ba3534e
Dominik Mendoza <dominick9753@gmail.com>
2021-04-29 11:45:14 -0500

    ae

--- Uribe/Source.cpp
+++ Uribe/Source.cpp
@@ -1,7 +1,8 @@
 #include "CInvasion.h"
 void main()
 {
-	const int MAXH = 30;
+	const int MAXH = 20;
+	Console::SetWindowSize(200, 40);
 	CInvasion* obj = new CInvasion();
 	obj->juego(MAXH);
 	delete obj;
